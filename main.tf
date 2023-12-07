1a8957851137a9753354f45d345eb8aaa42802cb
athulya <athulya@gmail.com>
2023-12-07 06:25:08 +0000

    remote-access security group created

--- main.tf
+++ main.tf
@@ -50,3 +50,34 @@ resource "aws_security_group" "http_access" {
     owner   = var.project_owner
   }
 }
+
+#=========remote-access==================
+
+resource "aws_security_group" "remote_access" {
+  name        = "${var.project_name}-${var.project_env}-remote-access"
+  description = "${var.project_name}-${var.project_env}-remote-access"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    name    = "${var.project_name}-${var.project_env}-remote-access"
+    project = var.project_name
+    env     = var.project_env
+    owner   = var.project_owner
+  }
+}
+
