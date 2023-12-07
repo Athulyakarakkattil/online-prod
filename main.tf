9b194a91c9403fb6b1a50d5d380d269a6ac50121
athulya <athulya@gmail.com>
2023-12-07 06:13:40 +0000

    ssh access created

---
+++ main.tf
@@ -0,0 +1,14 @@
+#=======ssh-keygen=================
+
+resource "aws_key_pair" "auth_key" {
+
+  key_name   = "${var.project_name}-${var.project_env}"
+  public_key = file("onlinekey.pub")
+
+  tags = {
+    Name    = "${var.project_name}-${var.project_env}"
+    project = var.project_name
+    env     = var.project_env
+    owner   = var.project_owner
+  }
+}
