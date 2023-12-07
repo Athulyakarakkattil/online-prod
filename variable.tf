1498da4ee09aa0c730f49af62f12c0aea652b6fb
athulya <athulya@gmail.com>
2023-12-07 04:56:43 +0000

    remote backend configured

---
+++ variable.tf
@@ -0,0 +1,32 @@
+variable "ami_id" {
+
+  type        = string
+  description = "ami id if the instance"
+  default     = "ami-02e94b011299ef128"
+}
+
+variable "instance_type" {
+
+  type        = string
+  description = "type of the instance"
+  default     = "t2.micro"
+}
+
+variable "project_name" {
+
+  type        = string
+  description = "Name of the project"
+  default     = "online"
+}
+
+variable "project_env" {
+  type        = string
+  description = "Environment of the project"
+  default     = "production"
+}
+
+variable "project_owner" {
+  type        = string
+  description = "Owner of the project"
+  default     = "athulya"
+}
