c5a3e6b55c1bc7d1a22b18dc743d31ee86a3c834
athulya <athulya@gmail.com>
2023-12-08 15:28:47 +0000

    elastic ip for frontend server

--- main.tf
+++ main.tf
@@ -106,14 +106,26 @@ resource "aws_instance" "frontend" {
 
 }
 
+#========elastic_ip_for_Ec2_instance======
+resource "aws_eip" "frontend" {
+   instance = aws_instance.frontend.id
+   domain = "vpc"
+   tags = {
+    Name    = "${var.project_name}-${var.project_env}"
+    project = var.project_name
+    env     = var.project_env
+    owner   = var.project_owner
+  }
+}
 
 #====Domain_record_creation================
 
 resource "aws_route53_record" "frontend" {
 
-  zone_id = var.hosted_zone_id
-  name    = "${var.hostname}.${var.hosted_zone_name}"
-  type    = "A"
-  ttl     = 300
-  records = [aws_instance.frontend.public_ip]
+    zone_id = var.hosted_zone_id
+    name    = "${var.hostname}.${var.hosted_zone_name}"
+    type    = "A"
+    ttl     = 300
+    records = [ aws_eip.frontend.public_ip ]
 }
+
